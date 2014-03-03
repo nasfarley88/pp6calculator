3ec1cdc83a803d2e914e2acb05e07a900f7ba130
Nathanael Farley <nasfarley88@gmail.com>
2014-03-03 15:43:20 +0000

    Tidied code to use oneFunctionToRuleThemAll

--- main.cxx
+++ main.cxx
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <climits>
 
 double multiply(double a, double b) {
   // Multiply two numbers and return the output
@@ -20,38 +21,57 @@ double subtract(double a, double b) {
   // Subtract two numbers and return the output
   return a-b;
 }
-  
+
+bool cinChecker(bool x) {
+  std::cin.clear();
+  std::cin.ignore(INT_MAX, '\n');
+  if(x) {
+    std::cout << "Why are you feeding me garbage?" << std::endl;
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+bool oneFunctionToRuleThemAll(double (*func)(double,double), char operatorChar) {
+  double a, b;
+    
+  std::cout << "What is the first number you wish to operate? ";
+  std::cin >> a;
+  if(cinChecker(!std::cin)) {
+    return 0;
+  }
+    
+  std::cout << "What is the second number you wish to operate? ";
+  std::cin >> b;
+  if(cinChecker(!std::cin)) {
+    return 0;
+  }
+
+  std::cout << "The answer to " << a << " " << operatorChar << " " << b
+	    << " is " << (*func)(a,b) << std::endl;
+
+}
+
+
 int main(int argc, char *argv[]) {
   char decision = 'n';
 
   std::cout << "What operation would you like to perform? ";
   std::cin >> decision;
 
-  // TODO make it so that I can call a function from the
-  // function. Yeah. Function pointers?
   if(decision == 'm') {
-    double a, b;
-    
-    std::cout << "What is the first number you wish to muptiply? ";
-    std::cin >> a;
-    if(!std::cin) {
-      std::cout << "Why are you feeding me garbage?" << std::endl
-		<< "Goodbye." << std::endl;
-      return 1;
-    }
-    
-    std::cout << "What is the second number you wish to muptiply? ";
-    std::cin >> b;
-    if(!std::cin) {
-      std::cout << "Why are you feeding me garbage?" << std::endl
-		<< "Goodbye." << std::endl;
-      return 1;
-    }
-
-
-    std::cout << "The answer to " << a << "*" << b << " is " << multiply(a,b)
-	      << std::endl;
-
+    oneFunctionToRuleThemAll(&multiply,'*');
+  }
+  else if(decision == 's') {
+    oneFunctionToRuleThemAll(&subtract,'-');
+  }
+  else if(decision == 'a') {
+    oneFunctionToRuleThemAll(&add,'+');
+  }
+  else if(decision == 'd') {
+    oneFunctionToRuleThemAll(&divide,'/');
   }
   else {
     std::cout << "I did not understand your input. Goodbye!" << std::endl;
