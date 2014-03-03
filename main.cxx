87edb8243fb12d6f057c5b2529e4339d5d02c436
Nathanael Farley <nasfarley88@gmail.com>
2014-03-03 16:26:43 +0000

    made functions void (no need to return value when they cout the value themselves)

--- main.cxx
+++ main.cxx
@@ -2,38 +2,41 @@
 #include <cmath>
 #include <climits>
 
-double multiply(double a, double b) {
+void multiply(double a, double b) {
   // Multiply two numbers and return the output
   std::cout << "The answer to " << a << " * " << b
   	    << " is " << a*b << std::endl;
-  return a*b;
 }
 
-double divide(double a, double b) {
+void divide(double a, double b) {
   // Divide two numbers and return the output
   std::cout << "The answer to " << a << " / " << b
   	    << " is " << a/b << std::endl;
-  return a/b;
 }
 
-double add(double a, double b) {
+void add(double a, double b) {
   // Add two numbers and return the output
   std::cout << "The answer to " << a << " + " << b
   	    << " is " << a+b << std::endl;
-  return a+b;
 }
 
-double subtract(double a, double b) {
+void subtract(double a, double b) {
   // Subtract two numbers and return the output
   std::cout << "The answer to " << a << " - " << b
   	    << " is " << a-b << std::endl;
-  return a-b;
 }
 
-double xIntercept(double m, double c) {
+void xIntercept(double m, double c) {
   // For the equation y = mx + c, what is the value of the x intercept?
   std::cout << "The x intercept for the equation y =" << m << "x + " << c << " is " << -c/m;
-  return -c/m;
+}
+
+void solveQuadraticEquation(double a, double b, double c) {
+  std::cout << "y = 0 for y = ";
+  if(a!=1) std::cout << a;
+  std::cout << "x^2 + " << b << "x + " << c << " when" << std::endl
+	    << "x = " << (-b+sqrt(pow(b,2)-4*a*c))/(2*a) << std::endl
+	    << "x = " << (-b-sqrt(pow(b,2)-4*a*c))/(2*a) << std::endl;
 }
 
 bool cinChecker(bool x) {
@@ -48,9 +51,33 @@ bool cinChecker(bool x) {
   }
 }
 
-bool oneFunctionToRuleThemAll(double (*func)(double,double)) {
+bool oneFunctionToRuleThemAll(void (*func)(double,double)) {
   // This function prompts the user for variables to put into func(double,double)
   double a, b;
+    
+  std::cout << "What is the first number you wish to operate? ";
+  std::cin >> a;
+  if(cinChecker(!std::cin)) {
+    return 1;
+  }
+    
+  std::cout << "What is the second number you wish to operate? ";
+  std::cin >> b;
+  if(cinChecker(!std::cin)) {
+    return 1;
+  }
+
+  // Run the function with the specified inputs
+  (*func)(a,b);
+
+  return 0;
+}
+
+bool oneFunctionToRuleThemAll(void (*func)(double,double,double)) {
+  // This function prompts the user for variables to put into func(double,double,double)
+  //
+  // NB this is an overloaded function
+  double a, b, c;
     
   std::cout << "What is the first number you wish to operate? ";
   std::cin >> a;
@@ -61,11 +88,19 @@ bool oneFunctionToRuleThemAll(double (*func)(double,double)) {
   std::cout << "What is the second number you wish to operate? ";
   std::cin >> b;
   if(cinChecker(!std::cin)) {
-    return 0;
+    return 1;
+  }
+
+  std::cout << "What is the second number you wish to operate? ";
+  std::cin >> c;
+  if(cinChecker(!std::cin)) {
+    return 1;
   }
 
   // Run the function with the specified inputs
-  (*func)(a,b);
+  (*func)(a,b,c);
+
+  return 0;
 }
 
 
@@ -90,6 +125,9 @@ int main(int argc, char *argv[]) {
     }
     else if(decision == 'd') {
       oneFunctionToRuleThemAll(&divide);
+    }
+    else if(decision == 'q') {
+      oneFunctionToRuleThemAll(&solveQuadraticEquation);
     }
     else {
       std::cout << "I did not understand your input." << std::endl;
