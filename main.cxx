3eb3e31c4b16ec00b0144b0d891c427b61d8aa0a
Nathanael Farley <nasfarley88@gmail.com>
2014-03-03 16:17:25 +0000

    simplified code by putting cout into each function

--- main.cxx
+++ main.cxx
@@ -4,22 +4,36 @@
 
 double multiply(double a, double b) {
   // Multiply two numbers and return the output
+  std::cout << "The answer to " << a << " * " << b
+  	    << " is " << a*b << std::endl;
   return a*b;
 }
 
 double divide(double a, double b) {
   // Divide two numbers and return the output
+  std::cout << "The answer to " << a << " / " << b
+  	    << " is " << a/b << std::endl;
   return a/b;
 }
 
 double add(double a, double b) {
   // Add two numbers and return the output
+  std::cout << "The answer to " << a << " + " << b
+  	    << " is " << a+b << std::endl;
   return a+b;
 }
 
 double subtract(double a, double b) {
   // Subtract two numbers and return the output
+  std::cout << "The answer to " << a << " - " << b
+  	    << " is " << a-b << std::endl;
   return a-b;
+}
+
+double xIntercept(double m, double c) {
+  // For the equation y = mx + c, what is the value of the x intercept?
+  std::cout << "The x intercept for the equation y =" << m << "x + " << c << " is " << -c/m;
+  return -c/m;
 }
 
 bool cinChecker(bool x) {
@@ -34,7 +48,8 @@ bool cinChecker(bool x) {
   }
 }
 
-bool oneFunctionToRuleThemAll(double (*func)(double,double), char operatorChar) {
+bool oneFunctionToRuleThemAll(double (*func)(double,double)) {
+  // This function prompts the user for variables to put into func(double,double)
   double a, b;
     
   std::cout << "What is the first number you wish to operate? ";
@@ -49,9 +64,8 @@ bool oneFunctionToRuleThemAll(double (*func)(double,double), char operatorChar) 
     return 0;
   }
 
-  std::cout << "The answer to " << a << " " << operatorChar << " " << b
-	    << " is " << (*func)(a,b) << std::endl;
-
+  // Run the function with the specified inputs
+  (*func)(a,b);
 }
 
 
@@ -62,29 +76,29 @@ int main(int argc, char *argv[]) {
 
   do {
 
-  std::cout << "What operation would you like to perform? ";
-  std::cin >> decision;
+    std::cout << "What operation would you like to perform? ";
+    std::cin >> decision;
 
-  if(decision == 'm') {
-    oneFunctionToRuleThemAll(&multiply,'*');
-  }
-  else if(decision == 's') {
-    oneFunctionToRuleThemAll(&subtract,'-');
-  }
-  else if(decision == 'a') {
-    oneFunctionToRuleThemAll(&add,'+');
-  }
-  else if(decision == 'd') {
-    oneFunctionToRuleThemAll(&divide,'/');
-  }
-  else {
-    std::cout << "I did not understand your input. Goodbye!" << std::endl;
-  }
+    if(decision == 'm') {
+      oneFunctionToRuleThemAll(&multiply);
+    }
+    else if(decision == 's') {
+      oneFunctionToRuleThemAll(&subtract);
+    }
+    else if(decision == 'a') {
+      oneFunctionToRuleThemAll(&add);
+    }
+    else if(decision == 'd') {
+      oneFunctionToRuleThemAll(&divide);
+    }
+    else {
+      std::cout << "I did not understand your input." << std::endl;
+    }
 
-  do {
-    std::cout << "Do you want to do another calculation(y/n)? ";
-    std::cin >> userWantsToStay;
-  } while(userWantsToStay != 'y' && userWantsToStay != 'n');
+    do {
+      std::cout << "Do you want to do another calculation(y/n)? ";
+      std::cin >> userWantsToStay;
+    } while(userWantsToStay != 'y' && userWantsToStay != 'n');
     
   } while(userWantsToStay == 'y');
     
