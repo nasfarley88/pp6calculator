c7bef4f90b209b0d0cb9c8fac9a43fa42c0d0a10
Nathanael Farley <nasfarley88@gmail.com>
2014-03-18 09:34:10 +0000

    Planning for Particle class

---
+++ ParticleClass.mm
@@ -0,0 +1,79 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Particle class" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1395072657144"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Member variables" POSITION="right" ID="ID_791155506" CREATED="1395072657457" MODIFIED="1395072661712">
+<edge COLOR="#00ff00"/>
+<node TEXT="Four momentum" ID="ID_1212400006" CREATED="1395072917482" MODIFIED="1395072922838">
+<node TEXT="Based on four vector class?" ID="ID_1018794403" CREATED="1395072924837" MODIFIED="1395072981759"/>
+<node TEXT="Energy" ID="ID_1905248769" CREATED="1395072982168" MODIFIED="1395072987603"/>
+<node TEXT="Momentum" ID="ID_840429685" CREATED="1395072987995" MODIFIED="1395072992156">
+<node TEXT="three vector class (steal from someone else?)" ID="ID_561773965" CREATED="1395134790268" MODIFIED="1395134800780"/>
+</node>
+</node>
+<node TEXT="PDGID (which generates:)" ID="ID_1320361906" CREATED="1395134480194" MODIFIED="1395134494038">
+<node TEXT=" mass" ID="ID_1875183730" CREATED="1395134494827" MODIFIED="1395134498088"/>
+<node TEXT="charge" ID="ID_1614433380" CREATED="1395134499364" MODIFIED="1395134501231"/>
+<node TEXT="name" ID="ID_463321587" CREATED="1395134501926" MODIFIED="1395134505218"/>
+</node>
+<node TEXT="position?" ID="ID_1802982028" CREATED="1395134647823" MODIFIED="1395134655405">
+<node TEXT="depends if there is &apos;canvas&apos; on which to place the particle" ID="ID_79558505" CREATED="1395134656375" MODIFIED="1395134681479"/>
+</node>
+</node>
+<node TEXT="Member functions" POSITION="left" ID="ID_1553678283" CREATED="1395073045235" MODIFIED="1395073048209">
+<edge COLOR="#ff00ff"/>
+<node TEXT="get/set functions" ID="ID_397149957" CREATED="1395073049514" MODIFIED="1395073064500"/>
+<node TEXT="beta()" ID="ID_450436278" CREATED="1395134586096" MODIFIED="1395134633615">
+<node TEXT="gives the v/c value for the particle" ID="ID_1438939490" CREATED="1395134634385" MODIFIED="1395134643599"/>
+</node>
+</node>
+</node>
+</map>
